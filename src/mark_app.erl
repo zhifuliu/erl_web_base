5217d6311d93e0a02cc2db2ede999558c07b614a
刘志付 <zhifu@liuzhifudeMacBook-Pro.local>
2017-10-07 00:43:18 +0800

    rebar create_app 初始化项目

---
+++ src/mark_app.erl
@@ -0,0 +1,16 @@
+-module(mark_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    mark_sup:start_link().
+
+stop(_State) ->
+    ok.
