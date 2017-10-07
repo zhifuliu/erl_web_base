f862c6fa2f4521014723013e739efcab4a67a78f
刘志付 <zhifu@liuzhifudeMacBook-Pro.local>
2017-10-07 17:09:25 +0800

    对 gen_tcp 的接口返回值做判断处理，使用 case 语句

--- src/myapp_web.erl
+++ src/myapp_web.erl
@@ -8,28 +8,42 @@ start(Port) ->
     % 开启一个新线程来监听指定端口
     spawn(
         fun() ->
-            {ok, Sock} = gen_tcp:listen(Port, [{active, false}]),
-            % 监听成功后，交由 loop 函数来执行监听
-            loop(Sock)
+            case gen_tcp:listen(Port, [{active, false}]) of
+                {ok, Sock} ->
+                    % 监听成功后，交由 loop 函数来执行监听
+                    ?LOG_INFO("start myapp_web success", []),
+                    loop(Sock);
+                {error, Reason} ->
+                    ?LOG_INFO("start myapp_web fail, reason:~p", [Reason])
+            end
         end
     ).
 
 loop(Sock) ->
     % 监听客户端的请求，每来一个请求，改变本次请求的控制进程（新开的线程），交由 handle 来处理
-    ?LOG_INFO("~p", ["get request"]),
-    {ok, Conn} = gen_tcp:accept(Sock),
-    Handler = spawn(
-        fun() ->
-            handle(Conn)
-        end
-    ),
-    % 改变套接字控制进程，将本次套接字交由 Handler 处理，Handler 新开了一个线程，所以，可以立马接收下一个请求
-    gen_tcp:controlling_process(Conn, Handler),
+    case gen_tcp:accept(Sock) of
+        {ok, Conn} ->
+            ?LOG_INFO("get request", []),
+            Handler = spawn(
+                fun() ->
+                    handle(Conn)
+                end
+            ),
+            % 改变套接字控制进程，将本次套接字交由 Handler 处理，Handler 新开了一个线程，所以，可以立马接收下一个请求
+            gen_tcp:controlling_process(Conn, Handler);
+        {error, Reason} ->
+            ?LOG_INFO("gen_tcp accept error, reason:~p", [Reason])
+    end,
     loop(Sock).
 
 handle(Conn) ->
     % 每一个请求都会转到本函数来处理
-    gen_tcp:send(Conn, response("zhifu")),
+    case gen_tcp:send(Conn, response("zhifu")) of
+        ok ->
+            ?LOG_INFO("reponse success", []);
+        {error, Reason} ->
+            ?LOG_INFO("reponse fail, reason:~p", [Reason])
+    end,
     gen_tcp:close(Conn).
 
 response(Str) ->
