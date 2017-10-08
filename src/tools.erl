932630a11743eeebf077e6e523fe2e6bed1f6b76
刘志付 <zhifu@liuzhifudeMacBook-Pro.local>
2017-10-08 14:00:20 +0800

    获取变量类型函数

--- src/tools.erl
+++ src/tools.erl
@@ -3,7 +3,8 @@
 -include("settings.hrl").
 -export([
     format/2,
-    printRequestMsg/1
+    printRequestMsg/1,
+    getVariableType/1
 ]).
 
 format(Fmt, Params) ->
@@ -11,3 +12,33 @@ format(Fmt, Params) ->
 
 printRequestMsg(Req) ->
     ?LOG_INFO("method:~p raw_path:~p path:~p parse_qs:~p parse_post:~p parse_cookie:~p", [Req:get(method), Req:get(raw_path), Req:get(path), Req:parse_qs(), Req:parse_post(), Req:parse_cookie()]).
+
+% 获取变量类型
+getVariableType(Value) ->
+    % Data is_list:字符串也是数组
+    if
+        is_atom(Value) ->
+            atom;
+        is_binary(Value) ->
+            binary;
+        is_bitstring(Value) ->
+            bitstring;
+        is_boolean(Value) ->
+            boolean;
+        is_float(Value) ->
+            float;
+        is_function(Value) ->
+            function;
+        is_integer(Value) ->
+            intrger;
+        is_list(Value) ->
+            list;
+        is_port(Value) ->
+            port;
+        is_reference(Value) ->
+            reference;
+        is_tuple(Value) ->
+            tuple;
+        true ->
+            other
+    end.
